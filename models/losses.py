0397d0700612c9f243c378afae2671ebcdc816f2
Yun Liu <liuwuliuyun@gmail.com>
2019-12-23 15:10:56 +0800

    models

---
+++ models/losses.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+class HeatmapLoss(nn.Module):
+    def __init__(self):
+        super(HeatmapLoss, self).__init__()
+
+    def forward(self, pred, gt):
+        assert pred.size() == gt.size()
+        loss = ((pred - gt)**2)
+        loss = loss.sum(dim=3).sum(dim=2).sum(dim=1).mean() / 2.
+        return loss
