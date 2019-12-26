43b899c06f21327993f4fda337935514c9a0e06a
Yun Liu <liuwuliuyun@gmail.com>
2019-12-26 14:02:55 +0800

    test_dataset update

--- test_dataset.py
+++ test_dataset.py
@@ -10,3 +10,10 @@ dataloader = torch.utils.data.DataLoader(trainset, batch_size=arg.batch_size, sh
                                                  num_workers=arg.workers, pin_memory=True)
 for i in dataloader:
     ipdb.set_trace()
+    # i -> List of 6 items
+    # i[0] batch_size*256*256 [Grey Scale Head Images]
+    # i[1] batch_size*196 [Coords of X and Y]
+    # i[2] batch_size*13*64*64 [Heatmaps]
+    # i[3] batch_size*196 [Coords of X and Y]
+    # i[4] batch_size*4 [bboxes]
+    # i[5] [list of original image filenames]
